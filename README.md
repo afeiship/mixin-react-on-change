42c61bf1da40f4ba244f5b1735731a99355e0468
afeiship <1290657123@qq.com>
2023-11-23 18:15:41 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,28 +1,34 @@
-# mixin-react-on-change
-> OnChange mixin for react.
+# next-web-toolkits
+> Web toolkits for web based on next.
+
+[![version][version-image]][version-url]
+[![license][license-image]][license-url]
+[![size][size-image]][size-url]
+[![download][download-image]][download-url]
 
 ## installation
 ```bash
-npm install -S @jswork/mixin-react-on-change
+yarn add @jswork/next-web-toolkits
 ```
-
-## bugs
-- [ ] state when formData.xx_field
 
 ## usage
 ```js
-// create file in: mixins/on-change.js
-export default require('@jswork/mixin-react-on-change').default(nx);
+import '@jswork/next-web-toolkits';
 
-
-// <input onChange={this.onChangeToState.bind(this, 'user.nickname')}>
-// <input onChange={this.onChangeToState.bind(this, 'user.phone')}>
-
-// state:
-{
-  user:{
-    nickname:'',
-    phone:''
-  }
-}
+//DOCS here!
 ```
+
+## license
+Code released under [the MIT license](https://github.com/afeiship/next-web-toolkits/blob/master/LICENSE.txt).
+
+[version-image]: https://img.shields.io/npm/v/@jswork/next-web-toolkits
+[version-url]: https://npmjs.org/package/@jswork/next-web-toolkits
+
+[license-image]: https://img.shields.io/npm/l/@jswork/next-web-toolkits
+[license-url]: https://github.com/afeiship/next-web-toolkits/blob/master/LICENSE.txt
+
+[size-image]: https://img.shields.io/bundlephobia/minzip/@jswork/next-web-toolkits
+[size-url]: https://github.com/afeiship/next-web-toolkits/blob/master/dist/next-web-toolkits.min.js
+
+[download-image]: https://img.shields.io/npm/dm/@jswork/next-web-toolkits
+[download-url]: https://www.npmjs.com/package/@jswork/next-web-toolkits
--- src/index.js
+++ src/index.js
@@ -1,64 +1,58 @@
-import "@jswork/next-capitalize";
+import nx from '@jswork/next';
+import EventMitt from '@jswork/event-mitt';
 
-const STATE = "state";
-const MEMORY = "memory";
-const LOCAL = "local";
-const SESSION = "session";
+// classes
+import '@jswork/next-local-storage';
+import '@jswork/next-session-storage';
 
-export default function () {
-  return class {
-    onChangeToState(inPath, inEvent) {
-      const { value } = inEvent.target;
-      const state = this.state;
-      nx.set(state, inPath, value);
-      return new Promise((resolve) => {
-        this.setState(state, () => {
-          resolve(state);
-        });
-      });
+// packages
+import '@jswork/next-camelize';
+import '@jswork/next-capitalize';
+import '@jswork/next-classify';
+import '@jswork/next-camelize';
+import '@jswork/next-compact-object';
+import '@jswork/next-deep-assign';
+import '@jswork/next-deep-clone';
+import '@jswork/next-deep-each';
+import '@jswork/next-deep-equal';
+import '@jswork/next-empty';
+import '@jswork/next-get2get';
+import '@jswork/next-is-empty-object';
+import '@jswork/next-is-plain-object';
+import '@jswork/next-kebab-case';
+import '@jswork/next-key-map';
+import '@jswork/next-param';
+import '@jswork/next-qs';
+import '@jswork/next-sets';
+import '@jswork/next-json';
+
+const defaults = { prefix: 'nak' };
+
+const NxWebToolkits = nx.declare('nx.WebToolkits', {
+  statics: {
+    create: function (inOptions) {
+      return new this(inOptions);
     }
-
-    onChangeToMemory(inPath, inValue) {
-      return this.onChangeTo(MEMORY, inPath, inValue);
+  },
+  methods: {
+    init: function (inOptions) {
+      this.options = nx.mix(null, defaults, inOptions);
+      this.initLocal();
+      this.initEvent();
+    },
+    initLocal: function () {
+      const { prefix } = this.options;
+      nx.sets({ $local: new nx.LocalStorage(prefix) });
+      nx.sets({ $session: new nx.SessionStorage(prefix) });
+    },
+    initEvent: function () {
+      nx.sets({ $event: nx.mix(null, EventMitt) });
     }
+  }
+});
 
-    onChangeToLocal(inPath, inValue) {
-      return this.onChangeTo(LOCAL, inPath, inValue);
-    }
-
-    onChangeToSession(inPath, inValue) {
-      return this.onChangeTo(SESSION, inPath, inValue);
-    }
-
-    onChangesToState(inObject) {
-      return this.onChangesTo(STATE, inObject);
-    }
-
-    onChangesToMemory(inObject) {
-      return this.onChangesTo(MEMORY, inObject);
-    }
-
-    onChangesToLocal(inObject) {
-      return this.onChangesTo(LOCAL, inObject);
-    }
-
-    onChangesToSession(inObject) {
-      return this.onChangesTo(SESSION, inObject);
-    }
-
-    onChangesTo(inType, inObject) {
-      const promiseList = nx.map(inObject, (key, value) => {
-        return this[`onChangeTo${nx.capitalize(inType)}`](key, value);
-      });
-      return Promise.all(promiseList);
-    }
-
-    onChangeTo(inType, inPath, inEvent) {
-      const data = nx[`$${inType}`];
-      const { value } = inEvent.target;
-      nx.set(data, inPath, value);
-      nx[`$${inType}`] = data;
-      return Promise.resolve(data);
-    }
-  };
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = NxWebToolkits;
 }
+
+export default NxWebToolkits;
