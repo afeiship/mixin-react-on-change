279613712429f4c5dd78ff96996aa111a6bbf070
afeiship <1290657123@qq.com>
2024-01-10 17:10:29 +0800

    feat: add nx. typing

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,7 @@ interface NxStatic {
   WebToolkits: {
     create: (options: any) => any;
   };
+  $global: any;
   $local: import('@jswork/next-local-storage');
   $session: import('@jswork/next-session-storage');
   $event: import('@jswork/event-mitt').EventMitt;
