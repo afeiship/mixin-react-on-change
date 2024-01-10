a879351372ede68b50f1a03bfd42e65fd3f02aa7
afeiship <1290657123@qq.com>
2024-01-10 17:04:53 +0800

    fix: typing err

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
 interface NxWebToolkitOptions {
   prefix?: string;
 }
+
 interface NxStatic {
   WebToolkits: {
     create: (options: any) => any;
-  }
+  };
+  $local: import('@jswork/next-local-storage');
+  $session: import('@jswork/next-session-storage');
+  $event: import('@jswork/event-mitt').EventMitt;
 }
